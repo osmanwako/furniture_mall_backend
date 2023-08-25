1a72dcffa80f5eca22b20f2fccc1a03fa5e19c09
OsmanFP <osmanwako411@gmail.com>
2023-08-25 18:45:51 +0300

    Devise and JWT token Authentication setup

---
+++ db/migrate/20230825083636_devise_create_users.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :full_name, null: false, default: ""
+      t.string :email, null: false, default: ""
+      t.string :role, null: false, default: "customer"
+      t.string :profile, null: false, default: "avatar.png"
+      t.string :jti, null: false
+      t.string :encrypted_password, null: false, default: ""
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email, unique: true
+    add_index :users, :reset_password_token, unique: true
+    add_index :users, :jti, unique: true
+  end
+end
